8de6f6cc173021ad5ce7b6f30f5207726505bd1a
Rachel Miles <milesrachel9@gmail.com>
2018-02-09 17:20:50 -0700

    added answers and main module

--- answers.rb
+++ answers.rb
@@ -1,2 +1,9 @@
-class answers
-  
+class Answers
+  attr_reader :answers_list
+  def initialize
+    @answers_list = [{response: 'You better believe it!', color: blue}, {response: "Absolutely", color: blue}, {response: "YAS", color: blue},
+    {response: "You could bet your life on it", color: blue}, {response: "chyeaaah", color: blue}, {response: "Perhaps", color: green},
+    {response: "Maybe",  color: green}, {response: "It's possible!", color: green} , {response: ". . . .", color: green}, {response: "I have no idea", color: green}
+    {response: "NO WAY JOSÈ" color: red} {response: "Nope", color: red}, {response: "Naaaah", color: red}, {response: "That's a no", color: red} {response: "Nah", color: red}]
+  end
+end
--- main.rb
+++ main.rb
@@ -1,13 +1,20 @@
 require "pry"
+require "colorize"
 require_relative "answers"
 
-class eight_ball
+class Eight_ball
   attr_accessor :answers
 
   def initialize
     @answers = answers.new
+    greeting
+  end
+
+  def greeting
     puts "I am a magical digital 8-ball!!"
     puts "Type a question to receive an answer, or type QUIT to exit."
+    question = gets.strip
+    answer = @answers.answers_list.sample
   end
 
   def exit_game
@@ -15,4 +22,43 @@ class eight_ball
     exit
   end
 
-eight_ball
+Eight_ball.new
+
+
+require_relative "answers"
+require "colorize"
+
+class Main
+    attr_accessor :answers
+
+    def initialize
+        @answers = Answers.new
+        welcome
+    end
+
+    def welcome
+        puts "Welcome to the Magic Eight Ball!"
+        puts "To exit at any time type 'exit'"
+        puts "Ask me a question!"
+        question = gets.strip
+        answer = @answers.all.sample
+        if answer[:color] === "red"
+            puts "#{answer[:response]}".colorize(:red)
+        elsif answer[:color] === "green"
+            puts "#{answer[:response]}".colorize(:green)
+        else
+            puts "#{answer[:response]}".colorize(:yellow)
+        end
+    end
+
+    def exit_menu
+        puts "Confirm exit by typing 'exit'"
+        case
+        when gets.strip.downcase
+            puts "Goodbye!"
+            exit
+        end
+    end
+end
+
+Main.new
